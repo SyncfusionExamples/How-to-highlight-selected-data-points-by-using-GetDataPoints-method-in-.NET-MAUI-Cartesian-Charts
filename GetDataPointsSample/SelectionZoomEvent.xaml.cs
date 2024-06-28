04bb8f61207fce7400879aad33a83068ff96e68b
Sowndharya1911 <Sowndharya.Selladurai@syncfusion.com>
2024-06-28 17:19:16 +0530

    Updated sample

---
+++ GetDataPointsSample/SelectionZoomEvent.xaml.cs
@@ -0,0 +1,48 @@
+using Syncfusion.Maui.Charts;
+
+namespace GetDataPointsSample;
+
+public partial class SelectionZoomEvent : ContentPage
+{
+    public SelectionZoomEvent()
+    {
+        InitializeComponent();
+    }
+    private void Chart_SelectionZoomEnd(object sender, ChartSelectionZoomEventArgs e)
+    {
+        primaryAxis.ZoomFactor = 1;
+        primaryAxis.ZoomPosition = 0;
+        secondaryAxis.ZoomFactor = 1;
+        secondaryAxis.ZoomPosition = 0;
+    }
+
+    private void Chart_SelectionZoomDelta(object sender, ChartSelectionZoomDeltaEventArgs e)
+    {
+        var selectedIndexes = new List<int>();
+
+        if (cartesianChart is SfCartesianChart)
+        {
+            foreach (var series in cartesianChart.Series)
+            {
+                if (series is ScatterSeries scatterSeries)
+                {
+
+                    var rect = new Rect(e.ZoomRect.X - cartesianChart.SeriesBounds.Left, e.ZoomRect.Y, e.ZoomRect.Width, e.ZoomRect.Height);
+                    var dataPoints = scatterSeries.GetDataPoints(rect);
+
+                    if (dataPoints != null && viewModel != null)
+                    {
+                        for (int i = 0; i < viewModel.Data.Count; i++)
+                        {
+                            if (dataPoints.Contains(viewModel.Data[i]))
+                                selectedIndexes.Add(i);
+                        }
+                        scatterSeries.SelectionBehavior.SelectedIndexes = selectedIndexes;
+                    }
+                }
+            }
+        }
+
+    }
+}
+
